4461c84bc17c9ecc15d6f547eeef4bb8e98b2022
you-love-lucy <fjones96@ivytech.edu>
2025-12-21 10:20:06 -0500

    module 7 changes

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlPatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
